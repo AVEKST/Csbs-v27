d7566db83f9dc9f7180be4493b56fe827889c29b
AVEKST <155233293+AVEKST@users.noreply.github.com>
2024-02-29 22:38:21 +0100

    Update

---
+++ shop.py
@@ -0,0 +1,80 @@
+class Shop:
+    """
+   
+
+
+
+    offers = [
+
+        {
+            'ID': 6,
+            'OfferTitle': 'SPECIAL OFFER',
+            'Cost': 20,
+            'Multiplier': 1,
+            'SkinID': 0,
+            'ShopType': 0,
+            'ShopDisplay': 0,
+            'Timer': 99999
+        },
+
+        {
+            'ID': 10,
+            'OfferTitle': 'SPECIAL OFFER',
+            'Cost': 1500,
+            'Multiplier': 1,
+            'SkinID': 0,
+            'ShopType': 3,
+            'ShopDisplay': 0,
+            'Timer': 99999
+        },
+
+
+    ]
+
+
+    gold = [
+        {
+            'Cost': 20,
+            'Amount': 150
+        },
+
+        {
+            'Cost': 50,
+            'Amount': 400
+        },
+
+        {
+            'Cost': 140,
+            'Amount': 1200
+        },
+
+        {
+            'Cost': 280,
+            'Amount': 2600
+        },
+
+    ]
+
+    boxes = [
+        {
+            'Name': 'Big Box',
+            'Cost': 30,
+            'Multiplier': 3
+        },
+
+        {
+            'Name': 'Mega Box',
+            'Cost': 80,
+            'Multiplier': 10
+        }
+
+    ]
+
+
+    token_doubler = {
+
+        'Cost': 50,
+        'Amount': 1000
+    }
+
+
